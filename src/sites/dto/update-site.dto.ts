ff51e72a6eadeb12179fdae5a21cafbf290840d7
Kamil Pawlak <hello@kamilpawlak.com>
2026-04-19 16:09:02 +0200

    fix(sites): use PartialType from @nestjs/swagger to preserve Swagger metadata

--- src/sites/dto/update-site.dto.ts
+++ src/sites/dto/update-site.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateSiteDto } from './create-site.dto';
 
 export class UpdateSiteDto extends PartialType(CreateSiteDto) {}
