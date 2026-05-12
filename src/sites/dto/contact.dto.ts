b1ea7395c34bb8b6afc8216b97863c2228d1d698
Kamil Pawlak <hello@kamilpawlak.com>
2026-05-12 21:15:54 +0200

    fix(sites): allow empty strings for optional contact fields

--- src/sites/dto/contact.dto.ts
+++ src/sites/dto/contact.dto.ts
@@ -1,29 +1,29 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
+import { IsEmail, IsString, IsUrl, ValidateIf } from 'class-validator';
 
 export class ContactDto {
   @ApiPropertyOptional({ example: 'kamil@example.com' })
-  @IsOptional()
+  @ValidateIf((o) => o.email !== undefined && o.email !== '')
   @IsEmail()
   email?: string;
 
   @ApiPropertyOptional({ example: '+48 123 456 789' })
-  @IsOptional()
+  @ValidateIf((o) => o.phone !== undefined && o.phone !== '')
   @IsString()
   phone?: string;
 
   @ApiPropertyOptional({ example: 'https://linkedin.com/in/kamilpawlak' })
-  @IsOptional()
+  @ValidateIf((o) => o.linkedin !== undefined && o.linkedin !== '')
   @IsUrl()
   linkedin?: string;
 
   @ApiPropertyOptional({ example: 'https://github.com/kamilpawlak' })
-  @IsOptional()
+  @ValidateIf((o) => o.github !== undefined && o.github !== '')
   @IsUrl()
   github?: string;
 
   @ApiPropertyOptional({ example: 'https://kamilpawlak.com' })
-  @IsOptional()
+  @ValidateIf((o) => o.website !== undefined && o.website !== '')
   @IsUrl()
   website?: string;
 }
