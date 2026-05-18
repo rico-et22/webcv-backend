19cc5274a2b7385c5c1244c1c9a3ad0421986dce
Kamil Pawlak <hello@kamilpawlak.com>
2026-05-18 21:47:00 +0200

    chore: remove before-FE auth callback page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,6 @@
-import { readFileSync } from 'fs';
-import { join } from 'path';
-import { Controller, Get, Header } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { ApiExcludeController } from '@nestjs/swagger';
-import { Public } from './common/decorators/public.decorator';
-
-const HTML_PAGE = readFileSync(join(__dirname, 'auth-callback.html'), 'utf-8');
 
 @ApiExcludeController()
 @Controller()
-export class AppController {
-  @Public()
-  @Get()
-  @Header('Content-Type', 'text/html; charset=utf-8')
-  authCallback(): string {
-    return HTML_PAGE;
-  }
-}
+export class AppController {}
